6dea41e4ec37f4676461e98be0895d4cf91c44e9
Hyunji_ <hyunji1443@gmail.com>
2024-09-19 00:58:35 +0900

    Modify: localhost

--- server/gateway-server/src/main/java/com/bangez/gateway/config/WebSecurityConfig.java
+++ server/gateway-server/src/main/java/com/bangez/gateway/config/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://www.api.eomhyunji.bangez.com", "https://www.bangez.com", "https://www.taeho.site"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000"/*, "https://www.api.eomhyunji.bangez.com", "https://www.bangez.com", "https://www.taeho.site"*/));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
